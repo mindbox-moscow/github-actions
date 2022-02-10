f86a05b5890167280e16097a5fbdea37c9518224
Ekaterina Lysychenko <lysychenko@mindbox.ru>
2022-02-10 18:30:29 +0300

    fix

--- create-common-release/test.py
+++
@@ -1,8 +0,0 @@
-import json
-from sys import argv
-from os import system
-if __name__ == '__main__':
-    data_dict=json.loads(argv[1])
-    for dic in data_dict:
-        system("echo " + dic['project'] + " " + dic['env'])
-        system("octo --version")
