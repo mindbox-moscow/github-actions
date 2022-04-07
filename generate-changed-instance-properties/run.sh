a28c49e6245ab6984ac2a9ab5b0445f2e3df2a16
lysychenko <98894503+lysychenko@users.noreply.github.com>
2022-04-07 21:47:43 +0300

    delete debug

--- generate-changed-instance-properties/run.sh
+++ generate-changed-instance-properties/run.sh
@@ -7,8 +7,6 @@ IFS=', ' read -r -a dirs <<< "$2"
 
 function increment_version {
  local v=$1
- echo "name1 $1"
- echo "name2 $2"
  if [ -z $2 ]; then 
     local rgx='^((?:[0-9]+\.)*)([0-9]+)($)'
  else 
@@ -16,7 +14,6 @@ function increment_version {
     for (( p=`grep -o "\."<<<".$v"|wc -l`; p<$2; p++)); do 
        v+=.0; done; fi
  val=`echo -e "$v" | perl -pe 's/^.*'$rgx'.*$/$2/'`
- echo "version $v"
  echo "$v" | perl -pe s/$rgx.*$'/${1}'`printf %0${#val}s $(($val+1))`/
 }
 
@@ -24,21 +21,18 @@ get_tag_name_output=""
 function get_tag_name {
   get_tag_name_output=""
   local latest_sha=$(git rev-list -n 1 "$1-latest" 2>/dev/null)
-  echo "latest sha $latest_sha"
   if [[ -z "$latest_sha" ]]; then
     get_tag_name_output="1.0.0"
     return 11
   fi
 
   local latest_tags=($(git tag --points-at "$latest_sha" | grep "$1-[0-9]"))
-  echo "latest tags $latest_tags"
   if [[ "${#latest_tags[@]}" != 1 ]]; then
     exit 2
   fi
-  echo "latest_tags[0] ${latest_tags[0]}"
 
   local latest_version=$(echo ${latest_tags[0]} | sed "s/[^0-9]*//")
-  echo "latest version $latest_version"
+
   get_tag_name_output="$(increment_version $latest_version)"
 
   return 0
@@ -62,7 +56,6 @@ function is_base_changed {
 continue="no"
 first_time="no"
 
-echo "before tag $name"
 get_tag_name $name
 if [[ $? == 11 ]]; then
   first_time="yes"
@@ -72,10 +65,8 @@ version="$get_tag_name_output"
 
 if [[ $first_time == "no" ]]; then
   is_base_changed
-  echo "is_base_changed"
   if [[ $? == 0 ]]; then
     continue="yes"
-    echo "continue=yes"
   fi
 fi
 
