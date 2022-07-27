076cd075eadb7f7b65854a469be2d971c4735d45
Eugene Shtannikov <e.shtannikov@gmail.com>
2022-07-27 17:20:23 +0300

    fix: last commit

--- generate-release-changelog/entrypoint.sh
+++ generate-release-changelog/entrypoint.sh
@@ -1,21 +1,25 @@
 #!/bin/sh -l
 
-git clone --quiet https://github.com/$REPO &> /dev/null
+repo_url="https://github.com/$REPO"
+git clone --quiet $repo_url &> /dev/null
 
 git config --global --add safe.directory /github/workspace
 
 tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
-
 if [ "$tag" ]; then
   changelog=$(git log --oneline --no-decorate $tag..HEAD)
 else
   changelog=$(git log --oneline --no-decorate)
 fi
+lastcommit=$(git log $tag..HEAD --pretty=format:"%H" | head)
+lastcommit_url="$repo_url/commit/$lastcommit"
+lastcommit_hyperlink="[View latest commit in Github]($lastcommit_url)"
 
 echo $changelog
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A' - }"
 changelog=" - ${changelog//$'\r'/'%0D'}"
+changelog=$lastcommit_hyperlink$'%0A'$changelog
 
 echo "::set-output name=changelog::$changelog"
