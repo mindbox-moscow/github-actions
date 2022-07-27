df2edd91579603f16ba6e472dafb74eff06ff605
Eugene Shtannikov <e.shtannikov@gmail.com>
2022-07-27 19:28:16 +0300

    feat: refactor `lastcommit` logic

--- generate-release-changelog/entrypoint.sh
+++ generate-release-changelog/entrypoint.sh
@@ -8,18 +8,22 @@ git config --global --add safe.directory /github/workspace
 tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
 if [ "$tag" ]; then
   changelog=$(git log --oneline --no-decorate $tag..HEAD)
+  lastcommit=$(git log $tag..HEAD --pretty=format:"%H" --max-count=1)
 else
   changelog=$(git log --oneline --no-decorate)
+  lastcommit=$(git log --pretty=format:"%H" --max-count=1)
 fi
-lastcommit=$(git log $tag..HEAD --pretty=format:"%H" | head)
-lastcommit_url="$repo_url/commit/$lastcommit"
-lastcommit_hyperlink="[View latest commit in Github]($lastcommit_url)"
 
+echo "last commit: $lastcommit"
 echo $changelog
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A' - }"
 changelog=" - ${changelog//$'\r'/'%0D'}"
+
+lastcommit_url="$repo_url/commit/$lastcommit"
+lastcommit_hyperlink="[View latest commit in Github]($lastcommit_url)"
+
 changelog=$lastcommit_hyperlink$'<br/>'$changelog
 
 echo "::set-output name=changelog::$changelog"
